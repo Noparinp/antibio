f164f32a30b9b3161059a9c54c62ae684e12dc1e
Iris <noparin.sme@student.mahidol.edu>
2023-07-07 17:02:46 +0700

    update spectype+org

--- antibio_py1.py
+++ antibio_py1.py
@@ -4,38 +4,70 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
+
 os.chdir("E:/Download_E/")
 data = pd.read_excel("microbiology_data.xlsx")
 data_df = pd.DataFrame(data)
 
 organisms = data['organism']
 spctypes = data['spctype']
-
-# print(data_df.head())
-
 drugs_df = data_df.iloc[:, 4:]
-# print(drugs_df.head())
 
-# print unique value in all drugs_df
-# for i in range(0, len(drugs_df.columns)):
-    # print(drugs_df.iloc[:, i].unique())
-# result ['S' 'R' nan]
 
-# # if value is 'S' or 'R' then replace with -1 or 1 else 0
-# for i in range(0, len(drugs_df.columns)):
-#     drugs_df.iloc[:, i] = drugs_df.iloc[:, i].replace('S', -1)
-#     drugs_df.iloc[:, i] = drugs_df.iloc[:, i].replace('R', 1)
-#     drugs_df.iloc[:, i] = drugs_df.iloc[:, i].replace(np.nan, 0)
-
+########################Prepare data #######################################################
 # create dictionary for value s r or np.nan
 drugs_dict = {'S': -1, 'R': 1, np.nan: 0}
 # replace value in drugs_df
 drugs_df = drugs_df.replace(drugs_dict)
-# print(drugs_df.head())
+# sum of each column in drug dataframe
+drugs_sum = drugs_df.sum(axis=0) # type is series
+#add drug  sum row at the end of data_df
+data_df.loc['drug_sum'] = drugs_sum
+# print(data_df.tail())
 
-# calculate summation of each drug column of each organism and each spctype
-# example for organism 'Acinetobacter baumannii' and spctype 'Blood' and drug 'Amikacin' summation is 0
-print(data_df.groupby(['organism', 'spctype']).sum())
+
+#plot the sum of each column with label value at each bar
+# plt.figure(figsize=(10, 5))
+# plt.bar(drugs_sum.index, drugs_sum.values)
+# plt.xticks(rotation=90)
+# plt.ylabel('Number of Isolates')
+# plt.title('Number of Isolates Resistant to Each Antibiotic')
+# plt.show()
+
+########################################## data of each organism and specimen type##############################################
+# iterate thright the spctypes and organism and sum the value from drug_df
+# and create a new dataframe with the sum of each spctype and organism
+# spc_org_df = pd.DataFrame()
+# for spc in spctypes.unique():
+#     for org in organisms.unique():
+#         spc_org_df.loc[spc, org] = drugs_df[(spctypes == spc) & (organisms == org)].sum().sum()
+    
+
+# print(spc_org_df.head())
+# spc_org_df.to_csv('D:/AllResearch/R_learning/spc_org_df.csv')
+
+#########################################################################################
+# iterate thright the spctypes and organism and sum the value from drug_df 
+# and create a new dataframe with the sum of each spctype and organism and drug
+spc_org_drug_df = pd.DataFrame()
+for spc in spctypes.unique():
+    for org in organisms.unique():
+        for drug in drugs_df.columns:
+            spc_org_drug_df.loc[spc, org, drug] = drugs_df[(spctypes == spc) & (organisms == org)][drug].sum()
+
+print(spc_org_drug_df.head())
+spc_org_drug_df.to_csv('D:/AllResearch/R_learning/spc_org_drug_df.csv')
+
+
+    
+
+
+
+
+
+
+
+
 
        
 
